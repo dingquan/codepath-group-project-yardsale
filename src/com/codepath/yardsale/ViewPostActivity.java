ab904ec47b09bf2a104ca5a4c92571ce5d7965ae
shanthi <shanthijayasekar@gmail.com>
2014-07-06 01:23:07 -0700

    Item details Click

--- src/com/codepath/yardsale/ViewPostActivity.java
+++ src/com/codepath/yardsale/ViewPostActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class ViewPostActivity extends Activity {
 
@@ -11,5 +13,14 @@ public class ViewPostActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_post);
+		
 	}
+	public void onReply(View view) {
+        Toast.makeText(this, "Reply Clicked", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+	public void onCancel(View view) {
+        Toast.makeText(this, "Cancel Clicked", Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
