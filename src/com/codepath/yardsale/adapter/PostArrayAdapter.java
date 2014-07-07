6b6d4750a8fc2701048a0ce2e666355afedacbad
Quan Ding <qding.git@outlook.com>
2014-07-07 10:26:52 -0700

    layout adjustment in search result list view

--- src/com/codepath/yardsale/adapter/PostArrayAdapter.java
+++ src/com/codepath/yardsale/adapter/PostArrayAdapter.java
@@ -43,7 +43,11 @@ public class PostArrayAdapter extends ArrayAdapter<Post> {
 
 		ImageLoader imageLoader = ImageLoader.getInstance();
 		tvTitle.setText(post.getTitle());
-		tvDescription.setText(post.getDescription());
+		String description = post.getDescription();
+		if (description.length() > 50){
+			description = description.substring(0, 50) + "...";
+		}
+		tvDescription.setText(description);
 		tvPrice.setText(post.getPrice().toString());
 		tvLocation.setText(post.getContact().getAddress());
 		
