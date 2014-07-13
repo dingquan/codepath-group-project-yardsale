59fbd2e103cc6b8b2fd83d59d984c018e508d3f3
surbhi751986 <surbhi751986@gmail.com>
2014-07-13 03:10:16 -0700

    fixed images for ManagePostActivity

--- src/com/codepath/yardsale/adapter/AdArrayAdapter.java
+++ src/com/codepath/yardsale/adapter/AdArrayAdapter.java
@@ -1,10 +1,12 @@
 package com.codepath.yardsale.adapter;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import android.content.Context;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,6 +59,45 @@ public class AdArrayAdapter extends ArrayAdapter<Post> {
 		Date date = new Date(post.getCreatedAt());
 		String dateStr = DateFormat.getDateFormat(v.getContext()).format(date);
 		tvDate.setText(dateStr);
+		
+		ArrayList<String> postUrl = post.getImageUrl();
+		if(postUrl !=null){
+			Log.d("PosrArrayAdapter posrtUrl --->>>","null");
+		}else{
+			Log.d("PosrArrayAdapter posrtUrl --->>>","not null");
+
+		}
+		
+		if(postUrl != null && postUrl.size()>0){
+			String url =postUrl.get(0);
+			Log.d("PostArrayAdapter url string--->>",url);
+			imageLoader.displayImage(url, ivImage);
+			
+			
+		}else{
+			String ctgry = post.getCategory().name();
+			System.out.println("PostArrayAdapter-->>"+ctgry);
+			if(ctgry.equals("TOYS_GAMES")){
+				ivImage.setImageResource(R.drawable.ic_toys);
+			}else if(ctgry.equals("FURNITURE")){
+				ivImage.setImageResource(R.drawable.ic_furniture);
+			}else if(ctgry.equals("ELECTRONICS")){
+				ivImage.setImageResource(R.drawable.ic_electronics);
+			}else if(ctgry.equals("CLOTHING_ACCESSRIES")){
+				ivImage.setImageResource(R.drawable.ic_clothing);
+			}else if(ctgry.equals("BOOKS_MAGAZINES")){
+				ivImage.setImageResource(R.drawable.ic_books);
+			}else if(ctgry.equals("COMPUTERS")){
+				ivImage.setImageResource(R.drawable.ic_computers);
+			}else if(ctgry.equals("APPLIANCES")){
+				ivImage.setImageResource(R.drawable.ic_appliances);
+			}else if(ctgry.equals("CARS")){
+				ivImage.setImageResource(R.drawable.ic_cars);
+			}else{
+				ivImage.setImageResource(R.drawable.ic_cells);
+			}
+		}
+		
 
 		return v;
 	}
