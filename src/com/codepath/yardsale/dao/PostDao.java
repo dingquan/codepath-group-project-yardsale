1df013096ca252c5a2b3b0f4291038dcc906e925
surbhi751986 <surbhi751986@gmail.com>
2014-07-22 01:25:23 -0700

    resolve the bug for getting push notifications, now its working

--- src/com/codepath/yardsale/dao/PostDao.java
+++ src/com/codepath/yardsale/dao/PostDao.java
@@ -37,7 +37,7 @@ public class PostDao {
 		}
 		return postDao;
 	}
-	
+
 	public List<Post> findPostsBySearchCriteria(SearchCriteria criteria){
 		List<Post> posts = new ArrayList<Post>();
 		ParseQuery<ParsePost> query = buildQuery(criteria);
@@ -53,7 +53,7 @@ public class PostDao {
 		}
 		return posts;
 	}
-	
+
 	private ParseQuery<ParsePost> buildQuery(SearchCriteria c) {
 		ParseQuery<ParsePost> query = ParseQuery.getQuery(ParsePost.class);
 		if (c != null) {
@@ -112,47 +112,64 @@ public class PostDao {
 			savePost(post,null);
 		}
 	}
-	
+
 	public void savePost(Post post, String tag) {
 		ParsePost parsePost = new ParsePost(post);
-		parsePost.saveInBackground();
-		SearchCriteria criteria = new SearchCriteria();
-		criteria.setUserId(parsePost.getUserId());
-		criteria.setTitle(parsePost.getTitle());
-		criteria.setDescription(parsePost.getDescription());
-		List<Post> result = new ArrayList<Post>();
-		result = findPostsBySearchCriteria(criteria);
-		JSONObject data =  new JSONObject();
-		if(result.size()==1){
-			String postStr = JsonUtil.toJson(result.get(0));
-			Log.d("PostDao notification obj DEBUG", "view details of post: " + postStr);
-			//i.putExtra("post", postStr);
-			try {
-				data.put("post",postStr );
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		try {
+			parsePost.save();
+			SearchCriteria criteria = new SearchCriteria();
+			criteria.setUserId(parsePost.getUserId());
+			criteria.setTitle(parsePost.getTitle());
+			criteria.setDescription(parsePost.getDescription());
+			List<Post> result = new ArrayList<Post>();
+			result = findPostsBySearchCriteria(criteria);
+
+			for(int i = 0;i<result.size();i++){
+				System.out.println("result:"+ result.get(0).getTitle()+" "+result.get(i).getDescription());
+
 			}
-			 ParsePush push = new ParsePush();
-			 push.setChannel(tag);
-			 push.setData(data);
-			push.setMessage("Your wishList item "+tag+" has just arrived !!");
-			
-			
-		}else{
-			Log.d("PostDao savePost","more then  result");
+			JSONObject data =  new JSONObject();
+
+			if(result.size()==1){
+				String postStr = JsonUtil.toJson(result.get(0));
+				Log.d("PostDao notification obj DEBUG", "view details of post: " + postStr);
+				//i.putExtra("post", postStr);
+				try {
+					data.put("post",postStr );
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ParsePush push = new ParsePush();
+				push.setChannel(tag);
+				push.setData(data);
+				push.setMessage("Your wishList item "+tag+" has just arrived !!");
+				push.sendInBackground();
+
+
+			}else{
+				Log.d("PostDao savePost","more then  result");
+			}
+
+
+
+		} catch (ParseException e1) {
+			e1.printStackTrace();
 		}
-		
-		
-       
+
+
+
+
+
+
 	}
-	
+
 	public void saveWish(WishItems item) {
 		WishList wishItem = new WishList(item);
 		wishItem.saveInBackground();
-		
+
 	}
-	
+
 	/**
 	 * find a Post by its unique id
 	 * @param uid
@@ -173,79 +190,79 @@ public class PostDao {
 		ParsePost parsePost = new ParsePost(post);
 		parsePost.deleteInBackground();
 	}
-	
+
 	public List<Post> getDummyPosts(){
 		String postsData = 
-"[\n" + 
-"    {\n" + 
-"        \"userId\": \"1234567\",\n" + 
-"        \"title\": \"Thomas Train Set\",\n" + 
-"        \"description\": \"Choo choo! All aboard! This imaginative activity table with train set makes a fantastic gift for your little conductor. Compatible with Chuggington and Thomas & Friends train sets, this wooden activity table comes with a handy storage bin to collect all the bits and pieces after playtime is over — perfect for busy moms! Since the activity table is conveniently double-sided, kids can build their train sets on one side, and use the other side to color and play with arts and crafts. Easy to assemble, the 45-piece wooden train and activity table offers a great way to keep playtime centrally placed in one easy-to-clean location. The activity table with train set includes three trains, numerous buildings, trees, one police car and one ambulance. The train tracks are adjustable.\",\n" + 
-"        \"contact\": {\n" + 
-"            \"phone\": \"650-123-4567\",\n" + 
-"            \"address\": \"San Mateo, CA\"\n" + 
-"        },\n" + 
-"        \"price\": \"30\",\n" + 
-"        \"category\": \"TOYS_GAMES\",\n" + 
-"        \"status\": \"Active\",\n" + 
-"        \"location\": {\n" + 
-"            \"latitude\": 37,\n" + 
-"            \"longitude\": -122\n" + 
-"        },\n" + 
-"        \"createdAt\": 1404198000000\n" + 
-"    },\n" + 
-"    {\n" + 
-"        \"userId\": \"1234568\",\n" + 
-"        \"title\": \"2012 Toyota Camry\",\n" + 
-"        \"description\": \"I am selling my grandmother's 2005 Toyota Camry. My grandmother is the original owner of the vehicle and has being driving it since she bought it in 2005. The Camry currently has 169,000 miles. It has a clean title and has never been in an accident. The car has been well maintained as my grandmother had it regularly serviced at her local mechanic. I have the past two years worth of service records to show some of the work and maintenance that has been done. The car has also been serviced recently to ensure that it has no problems and is ready to sell. I have a copy of the carfax vehicle history report if you'd like to see it. The car has passed smog and I have the title in hand so it's ready to be sold. If you have any questions or would like to make an appointment to see the vehicle, please contact me at by call or text. If I do not answer, feel free to leave a message and I will get back to you. Thanks for looking.\",\n" + 
-"        \"contact\": {\n" + 
-"            \"phone\": \"650-123-4567\",\n" + 
-"            \"address\": \"San Francisco, CA\"\n" + 
-"        },\n" + 
-"        \"price\": \"15000\",\n" + 
-"        \"category\": \"CARS\",\n" + 
-"        \"status\": \"Active\",\n" + 
-"        \"location\": {\n" + 
-"            \"latitude\": 37,\n" + 
-"            \"longitude\": -122\n" + 
-"        },\n" + 
-"        \"createdAt\": 1404284400000\n" + 
-"    },\n" + 
-"    {\n" + 
-"        \"userId\": \"1234569\",\n" + 
-"        \"title\": \"***Whirlpool commercial Washer & Electric Dryer with hoses***\",\n" + 
-"        \"description\": \"Selling a perfect working Newer Style Whirlpool Commercial Washer & Electric dryer set both are Super capacity. Clean and ready to go. In immaculate condition only 16 months old. Still has plastic on it. with all hoses still hooked up in so you can see they Function properly and never been worked on.\\nAsking $380 for the set will only sell as a set only\",\n" + 
-"        \"contact\": {\n" + 
-"            \"phone\": \"650-123-4567\",\n" + 
-"            \"address\": \"San Mateo, CA\"\n" + 
-"        },\n" + 
-"        \"price\": \"380\",\n" + 
-"        \"category\": \"APPLIANCES\",\n" + 
-"        \"status\": \"Active\",\n" + 
-"        \"location\": {\n" + 
-"            \"latitude\": 37,\n" + 
-"            \"longitude\": -122\n" + 
-"        },\n" + 
-"        \"createdAt\": 1404198000000\n" + 
-"    },\n" + 
-"    {\n" + 
-"        \"userId\": \"1234570\",\n" + 
-"        \"title\": \"Apple 2008 BLACK Macbook-Newer 500GB@7K, NEW(Battery, OEM Chgr)\",\n" + 
-"        \"description\": \"This is a black Macbook 2008 2.1(Penryn 45nm)GHz 13.3\\\" Macbook with 2Gb Ram, with a NEW 5500mah Battery(w/3cycles), and a New Apple OEM L-Magsafe Charger. This one has the faster Bus, Louder Sound System, & Better Video & can utilize up to 6GB of Ram. Do Not confuse this with the older 2.16GHz(Late 2006-Mid 2007) models.\",\n" + 
-"        \"contact\": {\n" + 
-"            \"phone\": \"650-123-4567\",\n" + 
-"            \"address\": \"San Mateo, CA\"\n" + 
-"        },\n" + 
-"        \"price\": \"300\",\n" + 
-"        \"category\": \"COMPUTERS\",\n" + 
-"        \"status\": \"Active\",\n" + 
-"        \"location\": {\n" + 
-"            \"latitude\": 37,\n" + 
-"            \"longitude\": -122\n" + 
-"        },\n" + 
-"        \"createdAt\": 1404198000000\n" + 
-"    }\n" +  
-"]";
+				"[\n" + 
+						"    {\n" + 
+						"        \"userId\": \"1234567\",\n" + 
+						"        \"title\": \"Thomas Train Set\",\n" + 
+						"        \"description\": \"Choo choo! All aboard! This imaginative activity table with train set makes a fantastic gift for your little conductor. Compatible with Chuggington and Thomas & Friends train sets, this wooden activity table comes with a handy storage bin to collect all the bits and pieces after playtime is over — perfect for busy moms! Since the activity table is conveniently double-sided, kids can build their train sets on one side, and use the other side to color and play with arts and crafts. Easy to assemble, the 45-piece wooden train and activity table offers a great way to keep playtime centrally placed in one easy-to-clean location. The activity table with train set includes three trains, numerous buildings, trees, one police car and one ambulance. The train tracks are adjustable.\",\n" + 
+						"        \"contact\": {\n" + 
+						"            \"phone\": \"650-123-4567\",\n" + 
+						"            \"address\": \"San Mateo, CA\"\n" + 
+						"        },\n" + 
+						"        \"price\": \"30\",\n" + 
+						"        \"category\": \"TOYS_GAMES\",\n" + 
+						"        \"status\": \"Active\",\n" + 
+						"        \"location\": {\n" + 
+						"            \"latitude\": 37,\n" + 
+						"            \"longitude\": -122\n" + 
+						"        },\n" + 
+						"        \"createdAt\": 1404198000000\n" + 
+						"    },\n" + 
+						"    {\n" + 
+						"        \"userId\": \"1234568\",\n" + 
+						"        \"title\": \"2012 Toyota Camry\",\n" + 
+						"        \"description\": \"I am selling my grandmother's 2005 Toyota Camry. My grandmother is the original owner of the vehicle and has being driving it since she bought it in 2005. The Camry currently has 169,000 miles. It has a clean title and has never been in an accident. The car has been well maintained as my grandmother had it regularly serviced at her local mechanic. I have the past two years worth of service records to show some of the work and maintenance that has been done. The car has also been serviced recently to ensure that it has no problems and is ready to sell. I have a copy of the carfax vehicle history report if you'd like to see it. The car has passed smog and I have the title in hand so it's ready to be sold. If you have any questions or would like to make an appointment to see the vehicle, please contact me at by call or text. If I do not answer, feel free to leave a message and I will get back to you. Thanks for looking.\",\n" + 
+						"        \"contact\": {\n" + 
+						"            \"phone\": \"650-123-4567\",\n" + 
+						"            \"address\": \"San Francisco, CA\"\n" + 
+						"        },\n" + 
+						"        \"price\": \"15000\",\n" + 
+						"        \"category\": \"CARS\",\n" + 
+						"        \"status\": \"Active\",\n" + 
+						"        \"location\": {\n" + 
+						"            \"latitude\": 37,\n" + 
+						"            \"longitude\": -122\n" + 
+						"        },\n" + 
+						"        \"createdAt\": 1404284400000\n" + 
+						"    },\n" + 
+						"    {\n" + 
+						"        \"userId\": \"1234569\",\n" + 
+						"        \"title\": \"***Whirlpool commercial Washer & Electric Dryer with hoses***\",\n" + 
+						"        \"description\": \"Selling a perfect working Newer Style Whirlpool Commercial Washer & Electric dryer set both are Super capacity. Clean and ready to go. In immaculate condition only 16 months old. Still has plastic on it. with all hoses still hooked up in so you can see they Function properly and never been worked on.\\nAsking $380 for the set will only sell as a set only\",\n" + 
+						"        \"contact\": {\n" + 
+						"            \"phone\": \"650-123-4567\",\n" + 
+						"            \"address\": \"San Mateo, CA\"\n" + 
+						"        },\n" + 
+						"        \"price\": \"380\",\n" + 
+						"        \"category\": \"APPLIANCES\",\n" + 
+						"        \"status\": \"Active\",\n" + 
+						"        \"location\": {\n" + 
+						"            \"latitude\": 37,\n" + 
+						"            \"longitude\": -122\n" + 
+						"        },\n" + 
+						"        \"createdAt\": 1404198000000\n" + 
+						"    },\n" + 
+						"    {\n" + 
+						"        \"userId\": \"1234570\",\n" + 
+						"        \"title\": \"Apple 2008 BLACK Macbook-Newer 500GB@7K, NEW(Battery, OEM Chgr)\",\n" + 
+						"        \"description\": \"This is a black Macbook 2008 2.1(Penryn 45nm)GHz 13.3\\\" Macbook with 2Gb Ram, with a NEW 5500mah Battery(w/3cycles), and a New Apple OEM L-Magsafe Charger. This one has the faster Bus, Louder Sound System, & Better Video & can utilize up to 6GB of Ram. Do Not confuse this with the older 2.16GHz(Late 2006-Mid 2007) models.\",\n" + 
+						"        \"contact\": {\n" + 
+						"            \"phone\": \"650-123-4567\",\n" + 
+						"            \"address\": \"San Mateo, CA\"\n" + 
+						"        },\n" + 
+						"        \"price\": \"300\",\n" + 
+						"        \"category\": \"COMPUTERS\",\n" + 
+						"        \"status\": \"Active\",\n" + 
+						"        \"location\": {\n" + 
+						"            \"latitude\": 37,\n" + 
+						"            \"longitude\": -122\n" + 
+						"        },\n" + 
+						"        \"createdAt\": 1404198000000\n" + 
+						"    }\n" +  
+						"]";
 		JSONArray list;
 		List<Post> result = new ArrayList<Post>();
 		try {
@@ -268,7 +285,7 @@ public class PostDao {
 		ParseQuery<WishList> query = ParseQuery.getQuery(WishList.class);
 		query.whereEqualTo("userId", userId);
 		wishList = (ArrayList<WishList>) query.find();
-		
+
 		for(WishList eachWish : wishList){
 			System.out.println("PostDao eachWish-->>" + eachWish.getItem() + eachWish.getObjectId());
 			WishItems eachItem = eachWish.toWishItems();
@@ -284,13 +301,13 @@ public class PostDao {
 			WishList item = new WishList(eachitem);
 			System.out.println("postDao wishlist item to delete"+ item.getItem()+item.getObjectId());
 			item.deleteInBackground(new DeleteCallback() {
-				
+
 				@Override
 				public void done(ParseException arg0) {
 					System.out.println(arg0);
 				}
 			});
 		}
-		
+
 	}
 }
